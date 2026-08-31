9d499818ba9302f2ce510b1a15947fb08b1c6a64
Ahad Hasan <aha174862@gmail.com>
2026-08-31 23:32:31 +0600

    Update mail transporter configuration for Gmail

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -7,7 +7,9 @@ if (!process.env.MAIL_HOST || !process.env.MAIL_PORT || !process.env.EMAIL || !p
 }
 
 const transporter = nodemailer.createTransport({
-       service: 'gmail',
+   host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
       auth: {
         user: process.env.EMAIL,
         pass: process.env.APP_PASSWORD,
